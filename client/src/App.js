bf3dff021817057f2dc75ff45761f0355b3f1409
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-06-06 07:25:41 -0400

    develop: frontend/implemented the home page and added route of it

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
