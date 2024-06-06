23b58c1cb80ce1aa7662f231d4e39ed2210c1672
Shahriyar117 <shahriyar.rahim333@gmail.com>
2024-06-05 21:24:57 -0400

    feature: backend/implemented the jwt

--- server/src/modules/auth/auth.service.ts
+++ server/src/modules/auth/auth.service.ts
@@ -1,12 +1,16 @@
+import * as bcrypt from 'bcrypt';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
 
   async signUp(
     username: string,
@@ -43,6 +47,8 @@ export class AuthService {
 
   async signIn(user: User) {
     const payload = { username: user.username, sub: user._id };
-    return payload;
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
